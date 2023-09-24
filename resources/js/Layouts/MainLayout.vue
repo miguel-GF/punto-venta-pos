68b8ef518f28eb47c13439e7c83abd1ffef9e58e
Miguel <miguel.gf.130@gmail.com>
2023-09-23 20:18:38 -0600

    [MainLayout] Se cambia icono de listado de ventas

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -82,7 +82,7 @@ const opcionesSistema = ref([
 		{ label: "Entradas y Salidas", tag: "agregarMovimientoInventario", icon: "las la-exchange-alt" },
 	]},
 	{ seccion: "Ventas", opciones: [
-		{	label: "Ventas", tag: "ventas", icon: "las la-file-invoice-dollar" },
+		{	label: "Ventas", tag: "ventas", icon: "las la-list" },
 		{	label: "Nueva Venta", tag: "agregarVenta", icon: "las la-file-invoice-dollar" },
 	]},
 	{ seccion: "Clientes", opciones: [
