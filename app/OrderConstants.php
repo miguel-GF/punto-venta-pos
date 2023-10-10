d823da0e8d7195feecfa3e5fdebd3659b13700ae
Miguel <miguel.gf.130@gmail.com>
2023-10-09 21:44:10 -0600

    [Constantes] Se agregan constantes de ordenamiento

--- app/OrderConstants.php
+++ app/OrderConstants.php
@@ -4,6 +4,10 @@ namespace App;
 
 class OrderConstants
 {
+  const CLAVE_ASC = "clave_asc";
+  const CLAVE_DESC = "clave_desc";
+  const FOLIO_ASC = "folio_asc";
+  const FOLIO_DESC = "folio_desc";
   const NOMBRE_ASC = "nombre_asc";
   const NOMBRE_DESC = "nombre_desc";
 }
