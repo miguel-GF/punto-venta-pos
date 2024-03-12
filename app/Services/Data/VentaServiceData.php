17dfd6c6115ad77bc8196cf3a76378b1599d5213
Miguel <miguel.gf.130@gmail.com>
2024-03-11 20:33:01 -0600

    [Ventas] Se crea método para obtener el archivo pdf en base64 de la venta

--- app/Services/Data/VentaServiceData.php
+++ app/Services/Data/VentaServiceData.php
@@ -2,7 +2,9 @@
 
 namespace App\Services\Data;
 
+use App\Constants;
 use App\Repos\Data\VentaRepoData;
+use Illuminate\Support\Facades\DB;
 use stdClass;
 use Throwable;
 
@@ -39,5 +41,29 @@ class VentaServiceData
     } catch (Throwable $th) {
       throw $th;
     }
+  }
+
+  /**
+   * obtenerArchivoPdf
+   *
+   * @param  mixed $ventaId
+   * @return stdClass
+   */
+  public static function obtenerArchivoPdf($ventaId): stdClass
+  {
+    $ventaArchivoPdf = DB::table('ventas_archivos')->select('*')
+      ->where('venta_id', $ventaId)
+      ->where('status', Constants::ACTIVO_STATUS)
+      ->get()->first();
+    $pdfContent = $ventaArchivoPdf->archivo;
+    $filePath = public_path($ventaArchivoPdf->nombre);
+    file_put_contents($filePath, $pdfContent);
+    $pdfArchivo = file_get_contents($filePath);
+    $pdfBase64 = base64_encode($pdfArchivo);
+
+    $res = new stdClass();
+    $res->nombre = $ventaArchivoPdf->nombre;
+    $res->base64 = $pdfBase64;
+    return $res;
   }
 }
