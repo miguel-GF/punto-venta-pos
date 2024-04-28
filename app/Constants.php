506d867b621a6f3c645773ff1f278d2def028b77
miguel-GF <miguel.gf.130@gmail.com>
2024-04-27 21:52:22 -0600

    [Constantes] Se agregan nuevas constantes

--- app/Constants.php
+++ app/Constants.php
@@ -14,4 +14,7 @@ class Constants
   const VENTA_MOVIMIENTO_INVENTARIO_TIPO = "Venta";
   const CAT_FOLIO_GLOBAL_VENTA = "venta";
   const ERROR_DATOS_INVALIDOS = 422;
+
+  const TIPO_FOTO_NORMAL = 'Normal';
+  const TIPO_FOTO_THUMBNAIL = 'Thumbnail';
 }
